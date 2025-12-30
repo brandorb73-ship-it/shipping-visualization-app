c0571c281fb52631ebd06dd48ab2c738a0a00005
brandorb73-ship-it <brandorb73@gmail.com>
2025-12-30 21:39:51 +1100

    Delete api directory

--- api/login.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const { password } = JSON.parse(req.body);
-  if (password === process.env.APP_PASSWORD) {
-    res.status(200).json({ success: true });
-  } else {
-    res.status(401).json({ success: false });
-  }
-}
