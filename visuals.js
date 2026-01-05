f17ec9a02745c581f4858506e284db08c6817b66
brandorb73-ship-it <brandorb73@gmail.com>
2026-01-05 21:35:48 +1100

    Update visuals.js

--- visuals.js
+++ visuals.js
@@ -127,18 +127,17 @@ window.drawMap = function(groups, idx) {
                     <b>Importer:</b> ${f[idx("Importer")]} (${f[idx("Destination Country")]})<br>
                         <b>Ports:</b> ${f[idx("Origin Port") ] || 'N/A'} → ${f[idx("Destination Port")] || 'N/A'}
                     </div>
-                    <table class="popup-table" style="width:100%; border-collapse: collapse; table-layout: fixed;">
-                        <thead>
-                            <tr style="background: #f8fafc;">
-                                <th style="width:85px; text-align:left;">Date</th>
-                                <th style="width:35px;">Weight(Kg)</th>
-                                <th style="width:60px;">Amount($)</th>
-                                <th style="width:130px;">PRODUCT</th>
-                                <th style="width:40px;">Mode</th>
-                            </tr>
-                        </thead>
-                        <tbody>${tableRows}</tbody>
-                    </table>
+                   <table class="popup-table">
+    <thead>
+        <tr style="background: #f8fafc;">
+            <th style="width: 18%;">Date</th>
+            <th style="width: 15%;">Weight</th>
+            <th style="width: 15%;">Amount</th>
+            <th style="width: 42%;">PRODUCT</th> <th style="width: 10%;">Mode</th>
+        </tr>
+    </thead>
+    <tbody>${tableRows}</tbody>
+</table>
                 </div>`, { maxWidth: 420 });
         }
     });
